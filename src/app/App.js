93c961a69cd801649c7b00ecc0be6694434450d8
Brian.Jordan <brianjordan283@gmail.com>
2026-02-23 15:06:38 +0100

    add routes to router

--- src/app/App.js
+++ src/app/App.js
@@ -17,6 +17,9 @@ import "./App.css";
 const router = createBrowserRouter( createRoutesFromElements (
   <Route path="/" element={<Root />}>
     <Route path="about" element={<About />} />
+    <Route path="sign-up" element={<SignUp />} />
+    <Route path="articles" element={<Articles />} />
+    <Route path="categories" element={<Categories />} />
   </Route>))
 
 function App () {
